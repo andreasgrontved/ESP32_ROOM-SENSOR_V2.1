013977afa0ead8737501c4dd1bb53d2662a9b13f
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-10 22:06:43 +0100

    Update __init__.py

--- components/dfrobot_c1001/switch/__init__.py
+++ components/dfrobot_c1001/switch/__init__.py
@@ -1,47 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import ENTITY_CATEGORY_CONFIG
+from esphome.const import ENTITY_CATEGORY_CONFIG, CONF_ID, CONF_NAME
+
 from .. import CONF_DFROBOT_C1001_ID, DFRobotC1001Component
 
 DEPENDENCIES = ["dfrobot_c1001"]
 
 dfrobot_c1001_ns = cg.esphome_ns.namespace("dfrobot_c1001")
-DfrobotC1001Switch = dfrobot_c1001_ns.class_("DfrobotC1001Switch", switch.Switch, cg.Component, cg.Parented.template(DFRobotC1001Component))
+DFRobotC1001Switch = dfrobot_c1001_ns.class_(
+    "DFRobotC1001Switch", switch.Switch, cg.Component
+)
 
-# Define specific switch types
-SenC1001PowerSwitch = dfrobot_c1001_ns.class_("SenC1001PowerSwitch", DfrobotC1001Switch)
-SenC1001LedSwitch = dfrobot_c1001_ns.class_("SenC1001LedSwitch", DfrobotC1001Switch)
-
-# Define schema for each switch type
-_SWITCH_SCHEMA = (
-    switch.switch_schema(entity_category=ENTITY_CATEGORY_CONFIG)
-    .extend(
+CONFIG_SCHEMA = (
+    switch.SWITCH_SCHEMA.extend(
         {
+            cv.GenerateID(): cv.declare_id(DFRobotC1001Switch),
+            cv.Required(CONF_NAME): cv.string,
             cv.GenerateID(CONF_DFROBOT_C1001_ID): cv.use_id(DFRobotC1001Component),
         }
     )
-    .extend(cv.COMPONENT_SCHEMA)
 )
-
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.Optional("sensor_active"): _SWITCH_SCHEMA.extend(
-            {cv.GenerateID(): cv.declare_id(SenC1001PowerSwitch)}
-        ),
-        cv.Optional("turn_on_led"): _SWITCH_SCHEMA.extend(
-            {cv.GenerateID(): cv.declare_id(SenC1001LedSwitch)}
-        ),
-    }
-)
-
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_DFROBOT_C1001_ID])
-    
-    for key, switch_type in [("sensor_active", SenC1001PowerSwitch), ("turn_on_led", SenC1001LedSwitch)]:
-        if key in config:
-            var = await switch.new_switch(config[key])
-            await cg.register_component(var, config[key])
-            await cg.register_parented(var, parent)
-            cg.add(getattr(parent, f"set_{key}_switch")(var))
+    var = await switch.new_switch(config)
+    await cg.register_component(var, config)
+    await cg.register_parented(var, config[CONF_DFROBOT_C1001_ID])
