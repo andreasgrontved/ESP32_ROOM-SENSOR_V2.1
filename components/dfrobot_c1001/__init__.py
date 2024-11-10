4d6ab8c36b8928611affb2fc1b4ed88100925ff1
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-10 22:04:01 +0100

    update

--- components/dfrobot_c1001/__init__.py
+++ components/dfrobot_c1001/__init__.py
@@ -1,16 +1,37 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
+from esphome.const import CONF_ID, CONF_HEIGHT, CONF_SENSITIVITY, CONF_FALL_TIME
 from esphome.components import uart
-from esphome.const import CONF_ID
+
+CODEOWNERS = ["@andreasgrontved"]
+DEPENDENCIES = ["uart"]
+MULTI_CONF = True
 
 dfrobot_c1001_ns = cg.esphome_ns.namespace("dfrobot_c1001")
-DFRobotC1001Component = dfrobot_c1001_ns.class_("DFRobotC1001Component", cg.Component, uart.UARTDevice)
+DFRobotC1001Component = dfrobot_c1001_ns.class_("DFRobotC1001Component", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(DFRobotC1001Component),
-}).extend(uart.UART_DEVICE_SCHEMA)
+# Expose CONF_DFROBOT_C1001_ID for binary_sensor and switch modules
+CONF_DFROBOT_C1001_ID = "dfrobot_c1001_id"
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(DFRobotC1001Component),
+            cv.Optional(CONF_HEIGHT, default=270): cv.int_,
+            cv.Optional(CONF_FALL_TIME, default=5): cv.int_,
+            cv.Optional(CONF_SENSITIVITY, default=3): cv.int_range(min=0, max=3),
+        }
+    ).extend(uart.UART_DEVICE_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
+
+    cg.add(var.set_installation_height(config[CONF_HEIGHT]))
+    cg.add(var.set_fall_time(config[CONF_FALL_TIME]))
+    cg.add(var.set_sensitivity(config[CONF_SENSITIVITY]))
+
+# Register actions if needed
--- components/dfrobot_c1001/switch/__init__.py
+++ components/dfrobot_c1001/switch/__init__.py
@@ -1,33 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import ENTITY_CATEGORY_CONFIG, CONF_TYPE
-
+from esphome.const import ENTITY_CATEGORY_CONFIG
 from .. import CONF_DFROBOT_C1001_ID, DFRobotC1001Component
 
 DEPENDENCIES = ["dfrobot_c1001"]
 
 dfrobot_c1001_ns = cg.esphome_ns.namespace("dfrobot_c1001")
-DFRobotC1001Switch = dfrobot_c1001_ns.class_(
-    "DFRobotC1001Switch",
-    switch.Switch,
-    cg.Component,
-    cg.Parented.template(DFRobotC1001Component),
-)
+DfrobotC1001Switch = dfrobot_c1001_ns.class_("DfrobotC1001Switch", switch.Switch, cg.Component, cg.Parented.template(DFRobotC1001Component))
 
-C1001PowerSwitch = dfrobot_c1001_ns.class_("C1001PowerSwitch", DFRobotC1001Switch)
-C1001LedSwitch = dfrobot_c1001_ns.class_("C1001LedSwitch", DFRobotC1001Switch)
-C1001UartPresenceSwitch = dfrobot_c1001_ns.class_(
-    "C1001UartPresenceSwitch", DFRobotC1001Switch
-)
-C1001StartAfterBootSwitch = dfrobot_c1001_ns.class_(
-    "C1001StartAfterBootSwitch", DFRobotC1001Switch
-)
+# Define specific switch types
+SenC1001PowerSwitch = dfrobot_c1001_ns.class_("SenC1001PowerSwitch", DfrobotC1001Switch)
+SenC1001LedSwitch = dfrobot_c1001_ns.class_("SenC1001LedSwitch", DfrobotC1001Switch)
 
+# Define schema for each switch type
 _SWITCH_SCHEMA = (
-    switch.switch_schema(
-        entity_category=ENTITY_CATEGORY_CONFIG,
-    )
+    switch.switch_schema(entity_category=ENTITY_CATEGORY_CONFIG)
     .extend(
         {
             cv.GenerateID(CONF_DFROBOT_C1001_ID): cv.use_id(DFRobotC1001Component),
@@ -36,19 +24,13 @@ _SWITCH_SCHEMA = (
     .extend(cv.COMPONENT_SCHEMA)
 )
 
-CONFIG_SCHEMA = cv.typed_schema(
+CONFIG_SCHEMA = cv.Schema(
     {
-        "sensor_active": _SWITCH_SCHEMA.extend(
-            {cv.GenerateID(): cv.declare_id(C1001PowerSwitch)}
+        cv.Optional("sensor_active"): _SWITCH_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(SenC1001PowerSwitch)}
         ),
-        "turn_on_led": _SWITCH_SCHEMA.extend(
-            {cv.GenerateID(): cv.declare_id(C1001LedSwitch)}
-        ),
-        "presence_via_uart": _SWITCH_SCHEMA.extend(
-            {cv.GenerateID(): cv.declare_id(C1001UartPresenceSwitch)}
-        ),
-        "start_after_boot": _SWITCH_SCHEMA.extend(
-            {cv.GenerateID(): cv.declare_id(C1001StartAfterBootSwitch)}
+        cv.Optional("turn_on_led"): _SWITCH_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(SenC1001LedSwitch)}
         ),
     }
 )
@@ -56,7 +38,10 @@ CONFIG_SCHEMA = cv.typed_schema(
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_DFROBOT_C1001_ID])
-    var = await switch.new_switch(config)
-    await cg.register_component(var, config)
-    await cg.register_parented(var, parent)
-    cg.add(getattr(parent, f"set_{config[CONF_TYPE]}_switch")(var))
+    
+    for key, switch_type in [("sensor_active", SenC1001PowerSwitch), ("turn_on_led", SenC1001LedSwitch)]:
+        if key in config:
+            var = await switch.new_switch(config[key])
+            await cg.register_component(var, config[key])
+            await cg.register_parented(var, parent)
+            cg.add(getattr(parent, f"set_{key}_switch")(var))
