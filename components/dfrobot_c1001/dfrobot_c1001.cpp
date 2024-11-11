6a5a30a8030e40d8c6296bbeadc737a16e7c3f65
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-11 23:38:33 +0100

    update

--- components/dfrobot_c1001/dfrobot_c1001.cpp
+++ components/dfrobot_c1001/dfrobot_c1001.cpp
@@ -7,14 +7,7 @@ namespace dfrobot_c1001 {
 static const char *TAG = "dfrobot_c1001";
 
 DFRobotC1001::DFRobotC1001(uart::UARTComponent *parent)
-    : UARTDevice(parent), hu_(this->get_stream()) {
-  // Initialize sensors
-  this->presence_binary_sensor = new esphome::binary_sensor::BinarySensor();
-  this->motion_sensor = new esphome::sensor::Sensor();
-  this->movement_param_sensor = new esphome::sensor::Sensor();
-  this->respiration_rate_sensor = new esphome::sensor::Sensor();
-  this->heart_rate_sensor = new esphome::sensor::Sensor();
-}
+    : UARTDevice(parent), hu_(this->get_stream()) {}
 
 void DFRobotC1001::setup() {
   ESP_LOGI(TAG, "Initializing sensor");
@@ -36,31 +29,37 @@ void DFRobotC1001::setup() {
 }
 
 void DFRobotC1001::loop() {
+  // Read and publish presence information
   int presence = this->hu_.smHumanData(this->hu_.eHumanPresence);
   if (this->presence_binary_sensor != nullptr) {
     this->presence_binary_sensor->publish_state(presence == 1);
   }
 
+  // Read and publish motion information
   int motion = this->hu_.smHumanData(this->hu_.eHumanMovement);
   if (this->motion_sensor != nullptr) {
     this->motion_sensor->publish_state(motion);
   }
 
+  // Read and publish body movement parameters
   int movement_param = this->hu_.smHumanData(this->hu_.eHumanMovingRange);
   if (this->movement_param_sensor != nullptr) {
     this->movement_param_sensor->publish_state(movement_param);
   }
 
+  // Read and publish respiration rate
   int respiration_rate = this->hu_.getBreatheValue();
   if (this->respiration_rate_sensor != nullptr) {
     this->respiration_rate_sensor->publish_state(respiration_rate);
   }
 
+  // Read and publish heart rate
   int heart_rate = this->hu_.getHeartRate();
   if (this->heart_rate_sensor != nullptr) {
     this->heart_rate_sensor->publish_state(heart_rate);
   }
 
+  // Log the data
   ESP_LOGD(TAG, "Presence: %d, Motion: %d, Movement Param: %d, Respiration Rate: %d, Heart Rate: %d",
            presence, motion, movement_param, respiration_rate, heart_rate);
 }
--- components/dfrobot_c1001/dfrobot_c1001.h
+++ components/dfrobot_c1001/dfrobot_c1001.h
@@ -4,30 +4,28 @@
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
-#include "DFRobot_HumanDetection.h"  // Include your sensor library
+
+#include "DFRobot_HumanDetection.h"
 
 namespace esphome {
 namespace dfrobot_c1001 {
 
 class DFRobotC1001 : public uart::UARTDevice, public Component {
  public:
-  // Constructor
   DFRobotC1001(uart::UARTComponent *parent);
 
-  // Component lifecycle methods
   void setup() override;
   void loop() override;
   void dump_config() override;
 
   // Sensors to expose
-  esphome::binary_sensor::BinarySensor *presence_binary_sensor{nullptr};
-  esphome::sensor::Sensor *motion_sensor{nullptr};
-  esphome::sensor::Sensor *movement_param_sensor{nullptr};
-  esphome::sensor::Sensor *respiration_rate_sensor{nullptr};
-  esphome::sensor::Sensor *heart_rate_sensor{nullptr};
+  esphome::binary_sensor::BinarySensor *presence_binary_sensor = new esphome::binary_sensor::BinarySensor();
+  esphome::sensor::Sensor *motion_sensor = new esphome::sensor::Sensor();
+  esphome::sensor::Sensor *movement_param_sensor = new esphome::sensor::Sensor();
+  esphome::sensor::Sensor *respiration_rate_sensor = new esphome::sensor::Sensor();
+  esphome::sensor::Sensor *heart_rate_sensor = new esphome::sensor::Sensor();
 
  private:
-  // Instance of your sensor class
   DFRobot_HumanDetection hu_;
 };
 
