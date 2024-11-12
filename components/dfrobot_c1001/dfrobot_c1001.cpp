8323365b8880d5f345ea95ded5d195d78fbe0046
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-12 21:08:34 +0100

    update

--- components/dfrobot_c1001/dfrobot_c1001.cpp
+++ components/dfrobot_c1001/dfrobot_c1001.cpp
@@ -1,134 +1,22 @@
+#include "esphome/core/log.h"
 #include "dfrobot_c1001.h"
 
 namespace esphome {
 namespace dfrobot_c1001 {
 
-DFRobotC1001::DFRobotC1001(uart::UARTComponent *parent) : UARTDevice(parent) {
-  this->stream_ = this->get_stream();
+static const char *TAG = "dfrobot_c1001.component";
+
+void dfrobot_c1001::setup() {
+
 }
 
+void dfrobot_c1001::loop() {
 
-void DFRobotC1001::setup() {
-  ESP_LOGI(TAG, "Initializing DFRobot C1001 sensor...");
-
-  // Initialize the sensor
-  uint32_t start_time = millis();
-  while (this->begin() != 0) {
-    if (millis() - start_time > 10000) {
-      ESP_LOGE(TAG, "Sensor initialization failed after 10 seconds.");
-      break;
-    }
-    ESP_LOGW(TAG, "Sensor initialization failed, retrying...");
-    delay(1000);
-  }
-
-  ESP_LOGI(TAG, "Sensor initialized successfully");
-
-  // Configure the sensor as needed
-  if (this->configWorkMode(eSleepMode) != 0) {
-    ESP_LOGE(TAG, "Failed to set work mode to Sleep Mode");
-  } else {
-    ESP_LOGI(TAG, "Work mode set to Sleep Mode");
-  }
-
-  // Create and register sensors
-  this->presence_binary_sensor_ = new binary_sensor::BinarySensor();
-  this->presence_binary_sensor_->set_name("Presence");
-  this->presence_binary_sensor_->set_device_class("presence");
-  this->register_component(this->presence_binary_sensor_);
-
-  this->motion_sensor_ = new sensor::Sensor();
-  this->motion_sensor_->set_name("Motion");
-  this->register_component(this->motion_sensor_);
-
-  this->movement_param_sensor_ = new sensor::Sensor();
-  this->movement_param_sensor_->set_name("Movement Parameter");
-  this->register_component(this->movement_param_sensor_);
-
-  this->respiration_rate_sensor_ = new sensor::Sensor();
-  this->respiration_rate_sensor_->set_name("Respiration Rate");
-  this->respiration_rate_sensor_->set_unit_of_measurement("breaths/min");
-  this->respiration_rate_sensor_->set_icon("mdi:lungs");
-  this->register_component(this->respiration_rate_sensor_);
-
-  this->heart_rate_sensor_ = new sensor::Sensor();
-  this->heart_rate_sensor_->set_name("Heart Rate");
-  this->heart_rate_sensor_->set_unit_of_measurement("bpm");
-  this->heart_rate_sensor_->set_icon("mdi:heart-pulse");
-  this->register_component(this->heart_rate_sensor_);
 }
 
-void DFRobotC1001::loop() {
-  // Read and publish presence information
-  int presence = this->smHumanData(eHumanPresence);
-  if (this->presence_binary_sensor_ != nullptr) {
-    this->presence_binary_sensor_->publish_state(presence == 1);
-  }
-
-  // Read and publish motion information
-  int motion = this->smHumanData(eHumanMovement);
-  if (this->motion_sensor_ != nullptr) {
-    this->motion_sensor_->publish_state(motion);
-  }
-
-  // Read and publish movement parameter
-  int movement_param = this->smHumanData(eHumanMovingRange);
-  if (this->movement_param_sensor_ != nullptr) {
-    this->movement_param_sensor_->publish_state(movement_param);
-  }
-
-  // Read and publish respiration rate
-  int respiration_rate = this->getBreatheValue();
-  if (this->respiration_rate_sensor_ != nullptr) {
-    this->respiration_rate_sensor_->publish_state(respiration_rate);
-  }
-
-  // Read and publish heart rate
-  int heart_rate = this->getHeartRate();
-  if (this->heart_rate_sensor_ != nullptr) {
-    this->heart_rate_sensor_->publish_state(heart_rate);
-  }
-
-  // Log the data
-  ESP_LOGD(TAG, "Presence: %d, Motion: %d, Movement Param: %d, Respiration Rate: %d, Heart Rate: %d",
-           presence, motion, movement_param, respiration_rate, heart_rate);
+void dfrobot_c1001::dump_config(){
+    ESP_LOGCONFIG(TAG, "dfrobot_c1001");
 }
-
-void DFRobotC1001::dump_config() {
-  ESP_LOGCONFIG(TAG, "DFRobot C1001 Sensor:");
-  LOG_UART_DEVICE(this);
-
-  if (this->presence_binary_sensor_) {
-    LOG_BINARY_SENSOR("  ", "Presence", this->presence_binary_sensor_);
-  }
-  if (this->motion_sensor_) {
-    LOG_SENSOR("  ", "Motion", this->motion_sensor_);
-  }
-  if (this->movement_param_sensor_) {
-    LOG_SENSOR("  ", "Movement Parameter", this->movement_param_sensor_);
-  }
-  if (this->respiration_rate_sensor_) {
-    LOG_SENSOR("  ", "Respiration Rate", this->respiration_rate_sensor_);
-  }
-  if (this->heart_rate_sensor_) {
-    LOG_SENSOR("  ", "Heart Rate", this->heart_rate_sensor_);
-  }
-}
-
-// Implementations of the methods from the original library
-
-uint8_t DFRobotC1001::begin(void) {
-  uint8_t data = 0x0f;
-  uint8_t buf[10];
-  if (this->getData(0x01, 0x83, 1, &data, buf) == 0) {
-    return 0;
-  }
-  return 1;
-}
-
-// Include all other methods from your original DFRobot_HumanDetection.cpp file
-// Make sure to prefix them with 'DFRobotC1001::' and use 'this->' where appropriate
-// Ensure there are no references to 'hu_'
 
 }  // namespace dfrobot_c1001
 }  // namespace esphome
--- components/dfrobot_c1001/dfrobot_c1001.h
+++ components/dfrobot_c1001/dfrobot_c1001.h
@@ -1,108 +1,18 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/log.h"
 #include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace dfrobot_c1001 {
 
-static const char *const TAG = "dfrobot_c1001.component";
-
-#define TIME_OUT 5000  // 5 seconds
-
-// Command parsing states
-#define CMD_HEAD 0
-#define CMD_CONFIG 1
-#define CMD_CMD 2
-#define CMD_LEN_H 3
-#define CMD_LEN_L 4
-#define CMD_END_H 5
-#define CMD_END_L 6
-#define CMD_WHITE 7
-#define CMD_DATA 8
-
-// Sleep composite state data
-typedef struct {
-  uint8_t presence;           ///< Presence state
-  uint8_t sleepState;         ///< Sleep state
-  uint8_t averageRespiration; ///< Average respiration
-  uint8_t averageHeartbeat;   ///< Average heartbeat
-  uint8_t turnoverNumber;     ///< Turnover number
-  uint8_t largeBodyMove;      ///< Large body movement percentage
-  uint8_t minorBodyMove;      ///< Minor body movement percentage
-  uint8_t apneaEvents;        ///< Apnea events
-} sSleepComposite;
-
-// Sleep statistics query
-typedef struct {
-  uint8_t sleepQualityScore;      ///< Sleep quality score
-  uint16_t sleepTime;             ///< Sleep duration in minutes
-  uint8_t wakeDuration;           ///< Wake duration
-  uint8_t shallowSleepPercentage; ///< Shallow sleep duration percentage
-  uint8_t deepSleepPercentage;    ///< Deep sleep duration percentage
-  uint8_t timeOutOfBed;           ///< Time out of bed
-  uint8_t exitCount;              ///< Exit count
-  uint8_t turnOverCount;          ///< Turnover count
-  uint8_t averageRespiration;     ///< Average respiration
-  uint8_t averageHeartbeat;       ///< Average heartbeat
-  uint8_t apneaEvents;            ///< Apnea events
-} sSleepStatistics;
-
-class DFRobotC1001 : public uart::UARTDevice, public Component {
- public:
-  // Enumerations and typedefs
-  typedef enum {
-    eSleepMode = 0x02,
-    eFallingMode = 0x01,
-  } eWorkMode;
-
-  typedef enum {
-    eFALLLed = 0x03,
-    eHPLed = 0x04,
-  } eLed;
-
-  typedef enum {
-    eHumanPresence,    ///< Human presence query
-    eHumanMovement,    ///< Movement information query
-    eHumanMovingRange, ///< Movement distance, range 0~100
-    eHumanDistance,
-  } esmHuman;
-
-  // Constructor
-  DFRobotC1001(uart::UARTComponent *parent);
-
-  // Overrides
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
-
-protected:
-  const char *name_;
-  binary_sensor::BinarySensor *presence_binary_sensor_{nullptr};
-  sensor::Sensor *motion_sensor_{nullptr};
-  sensor::Sensor *movement_param_sensor_{nullptr};
-  sensor::Sensor *respiration_rate_sensor_{nullptr};
-  sensor::Sensor *heart_rate_sensor_{nullptr};
-
-
-  // Sensor communication methods
-  uint8_t begin(void);
-  uint8_t configWorkMode(eWorkMode mode);
-  uint8_t getWorkMode(void);
-  uint16_t smHumanData(esmHuman hm);
-  uint8_t getBreatheValue(void);
-  uint8_t getHeartRate(void);
-
-  // Private methods
-  uint8_t getData(uint8_t con, uint8_t cmd, uint16_t len, uint8_t *senData, uint8_t *retData);
-  uint8_t sumData(uint8_t len, uint8_t *buf);
-
-  // Stream for communication
-  Stream *stream_;
+class dfrobot_c1001 : public uart::UARTDevice, public Component {
+  public:
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
 };
+
 
 }  // namespace dfrobot_c1001
 }  // namespace esphome
