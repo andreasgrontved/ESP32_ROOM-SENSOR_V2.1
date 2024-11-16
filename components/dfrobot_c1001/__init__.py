78613f0eb6efeaf46afe1b3ac077a78b7555dd40
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-16 20:28:57 +0100

    Update __init__.py

--- components/dfrobot_c1001/__init__.py
+++ components/dfrobot_c1001/__init__.py
@@ -7,11 +7,15 @@ from esphome.const import CONF_ID
 dfrobot_ns = cg.esphome_ns.namespace('dfrobot_c1001')
 DFRobotHumanDetection = dfrobot_ns.class_('DFRobot_HumanDetection', cg.Component)
 
+# Define configuration parameters
+CONF_RX_PIN = "rx_pin"
+CONF_TX_PIN = "tx_pin"
+
 # Configuration schema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DFRobotHumanDetection),
-    cv.Required("rx_pin"): pins.gpio_input_pin_schema,
-    cv.Required("tx_pin"): pins.gpio_output_pin_schema,
+    cv.Required(CONF_RX_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_TX_PIN): pins.gpio_output_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA)
 
 # Code generation logic
@@ -19,6 +23,6 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    rx_pin = await cg.gpio_pin_expression(config["rx_pin"])
-    tx_pin = await cg.gpio_pin_expression(config["tx_pin"])
+    rx_pin = await cg.gpio_pin_expression(config[CONF_RX_PIN])
+    tx_pin = await cg.gpio_pin_expression(config[CONF_TX_PIN])
     cg.add(var.set_uart_pins(rx_pin, tx_pin))
