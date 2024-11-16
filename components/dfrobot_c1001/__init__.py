062d0368f0e87e50202dc729310e26856ebf69d4
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-16 22:22:30 +0100

    Update __init__.py

--- components/dfrobot_c1001/__init__.py
+++ components/dfrobot_c1001/__init__.py
@@ -6,46 +6,52 @@ from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY, DEVICE_CLASS_EMPTY
 DEPENDENCIES = ["uart"]
 
 dfrobot_c1001_ns = cg.esphome_ns.namespace("dfrobot_c1001")
-dfrobot_c1001 = dfrobot_c1001_ns.class_(
-    "dfrobot_c1001", cg.Component, uart.UARTDevice
+DFRobotC1001Component = dfrobot_c1001_ns.class_(
+    "DFRobotC1001Component", cg.PollingComponent, uart.UARTDevice
 )
 
+CONF_PRESENCE = "presence"
+CONF_MOVEMENT = "movement"
+CONF_MOVING_RANGE = "moving_range"
+CONF_BREATHE_VALUE = "breathe_value"
+CONF_HEART_RATE = "heart_rate"
+
 CONFIG_SCHEMA = (
-    cv.Schema({cv.GenerateID(): cv.declare_id(dfrobot_c1001),
-    cv.Optional("presence"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_EMPTY,
-        icon=ICON_EMPTY,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_EMPTY,
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(DFRobotC1001Component),
+        cv.Optional(CONF_PRESENCE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_EMPTY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
         ),
-    cv.Optional("movement"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_EMPTY,
-        icon=ICON_EMPTY,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_EMPTY,
+        cv.Optional(CONF_MOVEMENT): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_EMPTY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
         ),
-    cv.Optional("moving_range"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_EMPTY,
-        icon=ICON_EMPTY,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_EMPTY,
+        cv.Optional(CONF_MOVING_RANGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_EMPTY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
         ),
-    cv.Optional("breathe_value"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_EMPTY,
-        icon=ICON_EMPTY,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_EMPTY,
+        cv.Optional(CONF_BREATHE_VALUE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_EMPTY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
         ),
-    cv.Optional("heart_rate"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_EMPTY,
-        icon=ICON_EMPTY,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_EMPTY,
+        cv.Optional(CONF_HEART_RATE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_EMPTY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
         ),
-     })
+    })
     .extend(cv.COMPONENT_SCHEMA)
     .extend(uart.UART_DEVICE_SCHEMA)
-    
 )
 
 
@@ -54,18 +60,18 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    if "presence" in config:
-        sens = await sensor.new_sensor(config["presence"])
+    if CONF_PRESENCE in config:
+        sens = await sensor.new_sensor(config[CONF_PRESENCE])
         cg.add(var.set_presence_sensor(sens))
-    if "movement" in config:
-        sens = await sensor.new_sensor(config["movement"])
+    if CONF_MOVEMENT in config:
+        sens = await sensor.new_sensor(config[CONF_MOVEMENT])
         cg.add(var.set_movement_sensor(sens))
-    if "moving_range" in config:
-        sens = await sensor.new_sensor(config["moving_range"])
+    if CONF_MOVING_RANGE in config:
+        sens = await sensor.new_sensor(config[CONF_MOVING_RANGE])
         cg.add(var.set_moving_range_sensor(sens))
-    if "breathe_value" in config:
-        sens = await sensor.new_sensor(config["breathe_value"])
+    if CONF_BREATHE_VALUE in config:
+        sens = await sensor.new_sensor(config[CONF_BREATHE_VALUE])
         cg.add(var.set_breathe_value_sensor(sens))
-    if "heart_rate" in config:
-        sens = await sensor.new_sensor(config["heart_rate"])
+    if CONF_HEART_RATE in config:
+        sens = await sensor.new_sensor(config[CONF_HEART_RATE])
         cg.add(var.set_heart_rate_sensor(sens))
