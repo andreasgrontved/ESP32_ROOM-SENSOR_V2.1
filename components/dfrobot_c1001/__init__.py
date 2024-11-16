c5548c464557dad37f2d3e1d20d426e65340de66
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-16 20:01:33 +0100

    Update __init__.py

--- components/dfrobot_c1001/__init__.py
+++ components/dfrobot_c1001/__init__.py
@@ -1,15 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import uart, sensor
 from esphome.const import CONF_ID
 
+DEPENDENCIES = ["uart"]
+
 dfrobot_c1001_ns = cg.esphome_ns.namespace('dfrobot_c1001')
-DFRobotC1001 = dfrobot_c1001_ns.class_('DFRobotC1001', sensor.Sensor)
+DFRobotC1001 = dfrobot_c1001_ns.class_('DFRobotC1001', cg.PollingComponent, uart.UARTDevice)
 
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(DFRobotC1001),
-})
+CONFIG_SCHEMA = (
+    sensor.SENSOR_SCHEMA.extend({
+        cv.GenerateID(): cv.declare_id(DFRobotC1001),
+    })
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.init())
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
