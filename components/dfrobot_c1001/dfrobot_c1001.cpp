9abfe19a15428e092e0cc8ff52a4e3c0c4a756b3
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-16 22:04:26 +0100

    update

--- components/dfrobot_c1001/dfrobot_c1001.cpp
+++ components/dfrobot_c1001/dfrobot_c1001.cpp
@@ -1,41 +1,9 @@
-#include "esphome/core/log.h"
 #include "dfrobot_c1001.h"
 
 namespace esphome {
 namespace dfrobot_c1001 {
 
-static const char *TAG = "dfrobot_c1001";
-
-DFRobotC1001::DFRobotC1001(Stream *stream) : sensor_(stream) {}
-
-void DFRobotC1001::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up DFRobot C1001...");
-
-  if (this->sensor_.begin() != 0) {
-    ESP_LOGE(TAG, "Failed to initialize DFRobot C1001 sensor!");
-  } else {
-    ESP_LOGI(TAG, "Successfully initialized DFRobot C1001 sensor.");
-  }
-}
-
-void DFRobotC1001::loop() {
-  this->process_sensor_data();
-}
-
-void DFRobotC1001::dump_config() {
-  ESP_LOGCONFIG(TAG, "DFRobot C1001:");
-  LOG_UART_DEVICE(this);
-}
-
-void DFRobotC1001::process_sensor_data() {
-  int presence = this->sensor_.smHumanData(DFRobot_HumanDetection::eHumanPresence);
-  int movement = this->sensor_.smHumanData(DFRobot_HumanDetection::eHumanMovement);
-
-  ESP_LOGD(TAG, "Presence: %d, Movement: %d", presence, movement);
-
-  ESP_LOGD(TAG, "Breathe rate: %d", this->sensor_.getBreatheValue());
-  ESP_LOGD(TAG, "Heart rate: %d", this->sensor_.getHeartRate());
-}
+// Implementation details are in the header file
 
 }  // namespace dfrobot_c1001
 }  // namespace esphome
--- components/dfrobot_c1001/dfrobot_c1001.h
+++ components/dfrobot_c1001/dfrobot_c1001.h
@@ -1,22 +1,53 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
+#include "esphome.h"
 #include "DFRobot_HumanDetection.h"
 
 namespace esphome {
 namespace dfrobot_c1001 {
 
-class DFRobotC1001 : public uart::UARTDevice, public Component {
+class DFRobotC1001 : public PollingComponent {
  public:
-  explicit DFRobotC1001(Stream *stream);
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
+  DFRobotC1001() : PollingComponent(1000) {}
+
+  void setup() override {
+    Serial1.begin(115200, SERIAL_8N1, 17, 16);
+    hu = new DFRobot_HumanDetection(&Serial1);
+
+    while (hu->begin() != 0) {
+      delay(1000);
+    }
+
+    while (hu->configWorkMode(hu->eSleepMode) != 0) {
+      delay(1000);
+    }
+
+    hu->configLEDLight(hu->eHPLed, 1);
+    hu->sensorRet();
+  }
+
+  void update() override {
+    int presence = hu->smHumanData(hu->eHumanPresence);
+    int movement = hu->smHumanData(hu->eHumanMovement);
+    int moving_range = hu->smHumanData(hu->eHumanMovingRange);
+    int breathe_value = hu->getBreatheValue();
+    int heart_rate = hu->getHeartRate();
+
+    presence_sensor->publish_state(presence);
+    movement_sensor->publish_state(movement);
+    moving_range_sensor->publish_state(moving_range);
+    breathe_value_sensor->publish_state(breathe_value);
+    heart_rate_sensor->publish_state(heart_rate);
+  }
+
+  Sensor *presence_sensor = new Sensor();
+  Sensor *movement_sensor = new Sensor();
+  Sensor *moving_range_sensor = new Sensor();
+  Sensor *breathe_value_sensor = new Sensor();
+  Sensor *heart_rate_sensor = new Sensor();
 
  private:
-  DFRobot_HumanDetection sensor_;
-  void process_sensor_data();
+  DFRobot_HumanDetection *hu;
 };
 
 }  // namespace dfrobot_c1001
