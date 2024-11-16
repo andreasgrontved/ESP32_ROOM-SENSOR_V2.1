5dbe919bd7b5f5f3f189d64402edd9ef91aa9d16
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-16 18:19:00 +0100

    update

--- components/dfrobot_c1001/__init__.py
+++ components/dfrobot_c1001/__init__.py
@@ -1,51 +1,38 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome import automation
-from esphome.const import CONF_ID
+from esphome import pins
 
-# Namespace and component class
-dfrobot_c1001_ns = cg.esphome_ns.namespace("dfrobot_c1001")
-DFRobotC1001 = dfrobot_c1001_ns.class_("DFRobotC1001", cg.Component, uart.UARTDevice)
+DEPENDENCIES = ['uart']
 
-# Configuration options for each sub-sensor
-CONF_PRESENCE_SENSOR = "presence_sensor"
-CONF_MOVEMENT_SENSOR = "movement_sensor"
-CONF_MOVEMENT_PARAM_SENSOR = "movement_param_sensor"
-CONF_RESPIRATION_RATE_SENSOR = "respiration_rate_sensor"
-CONF_HEART_RATE_SENSOR = "heart_rate_sensor"
+dfrobot_c1001_ns = cg.esphome_ns.namespace('dfrobot_c1001')
+DFRobotC1001 = dfrobot_c1001_ns.class_('DFRobotC1001', uart.UARTDevice, cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DFRobotC1001),
-    cv.Optional(CONF_PRESENCE_SENSOR): sensor.sensor_schema(),
-    cv.Optional(CONF_MOVEMENT_SENSOR): sensor.sensor_schema(),
-    cv.Optional(CONF_MOVEMENT_PARAM_SENSOR): sensor.sensor_schema(),
-    cv.Optional(CONF_RESPIRATION_RATE_SENSOR): sensor.sensor_schema(),
-    cv.Optional(CONF_HEART_RATE_SENSOR): sensor.sensor_schema(),
+    cv.Optional('presence_sensor'): sensor.sensor_schema(),
+    cv.Optional('movement_sensor'): sensor.sensor_schema(),
+    cv.Optional('movement_param_sensor'): sensor.sensor_schema(),
+    cv.Optional('respiration_rate_sensor'): sensor.sensor_schema(),
+    cv.Optional('heart_rate_sensor'): sensor.sensor_schema(),
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
-
-    # Link each sub-sensor if defined
-    if CONF_PRESENCE_SENSOR in config:
-        sens = await sensor.new_sensor(config[CONF_PRESENCE_SENSOR])
+    if 'presence_sensor' in config:
+        sens = yield sensor.new_sensor(config['presence_sensor'])
         cg.add(var.set_presence_sensor(sens))
-
-    if CONF_MOVEMENT_SENSOR in config:
-        sens = await sensor.new_sensor(config[CONF_MOVEMENT_SENSOR])
+    if 'movement_sensor' in config:
+        sens = yield sensor.new_sensor(config['movement_sensor'])
         cg.add(var.set_movement_sensor(sens))
-
-    if CONF_MOVEMENT_PARAM_SENSOR in config:
-        sens = await sensor.new_sensor(config[CONF_MOVEMENT_PARAM_SENSOR])
+    if 'movement_param_sensor' in config:
+        sens = yield sensor.new_sensor(config['movement_param_sensor'])
         cg.add(var.set_movement_param_sensor(sens))
-
-    if CONF_RESPIRATION_RATE_SENSOR in config:
-        sens = await sensor.new_sensor(config[CONF_RESPIRATION_RATE_SENSOR])
+    if 'respiration_rate_sensor' in config:
+        sens = yield sensor.new_sensor(config['respiration_rate_sensor'])
         cg.add(var.set_respiration_rate_sensor(sens))
-
-    if CONF_HEART_RATE_SENSOR in config:
-        sens = await sensor.new_sensor(config[CONF_HEART_RATE_SENSOR])
+    if 'heart_rate_sensor' in config:
+        sens = yield sensor.new_sensor(config['heart_rate_sensor'])
         cg.add(var.set_heart_rate_sensor(sens))
+    yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
