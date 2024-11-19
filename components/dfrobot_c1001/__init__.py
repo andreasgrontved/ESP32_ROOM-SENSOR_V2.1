77b7f8524c34646475d074e738397998c349c508
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2024-11-19 22:29:30 +0100

    Update __init__.py

--- components/dfrobot_c1001/__init__.py
+++ components/dfrobot_c1001/__init__.py
@@ -1,11 +1,20 @@
-from esphome import automation
-from esphome.components import uart
-from esphome.const import CONF_ID
+import esphome.codegen as cg
 import esphome.config_validation as cv
-import logging
+from esphome.components import uart
+from esphome import automation
 
-DEPENDENCIES = ["uart"]
+# Declare namespace and class
+dfrobot_c1001_ns = cg.esphome_ns.namespace("dfrobot_c1001")
+DFRobotC1001 = dfrobot_c1001_ns.class_("DFRobotC1001", uart.UARTDevice, cg.Component)
 
-_LOGGER = logging.getLogger(__name__)
+# Define configuration schema
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(DFRobotC1001),
+}).extend(uart.UART_DEVICE_SCHEMA)
 
-_LOGGER.info("DFRobot C1001 Component loaded successfully")
+# Code generation logic
+async def to_code(config):
+    # Create an instance of the component
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
